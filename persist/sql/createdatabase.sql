c1fbe0d9ada179d053007b0a117474d6d0859cd7
merzouk <merzouk@gmail.com>
2016-08-31 21:56:46 +0200

    Update createdatabase.sql

--- persist/sql/createdatabase.sql
+++ persist/sql/createdatabase.sql
@@ -26,17 +26,4 @@ create table functionnal.Facture(
    primary key(id)
 );
 
-insert into functionnal.Person (nom, prenom, email) values ('MENHOUR', 'Merzouk', 'mmerzouk@gmail.com');
-insert into functionnal.Users (login, password, email) values ('mmenhour', 'pmerzouk', 'mmerzouk@gmail.com');
-insert into functionnal.Facture (numeroFacture, email, montant, factureReglee) values ('fac000000001', 'mmerzouk@gmail.com', 123.45, false);
-insert into functionnal.Facture (numeroFacture, email, montant, factureReglee) values ('fac000000002', 'mmerzouk@gmail.com', 125.45, true);
-
-insert into functionnal.Person (nom, prenom, email) values ('MENHOUR', 'Ryan', 'mryan@gmail.com');
-insert into functionnal.Users (login, password, email) values ('mryan', 'pryan', 'mryan@gmail.com');
-insert into functionnal.Facture (numeroFacture, email, montant, factureReglee) values ('fac000000001', 'mryan@gmail.com', 223.45, false);
-insert into functionnal.Facture (numeroFacture, email, montant, factureReglee) values ('fac000000002', 'mryan@gmail.com', 10.45, true);
-
-insert into functionnal.Person (nom, prenom, email) values ('MENHOUR', 'Assia', 'massia@gmail.com');
-insert into functionnal.Users (login, password, email) values ('massia', 'passia', 'massia@gmail.com');
-insert into functionnal.Facture (numeroFacture, email, montant, factureReglee) values ('fac000000001', 'massia@gmail.com', 22.45, true);
-insert into functionnal.Facture (numeroFacture, email, montant, factureReglee) values ('fac000000002', 'massia@gmail.com', 105.45, true);
+-- Insert your data for the all tests.
